65d1e8f6939ad5ce102868e17a2adb5826eea9e1
miguelangelmuenala <49161695+miguelangelmuenala@users.noreply.github.com>
2023-06-20 10:10:54 -0500

    Proyecto casi terminado
    Solo falta la tabla de puntos, Sigan viendo...

---
+++ My project JuegosInteractivos/Assets/Scripts/DragonCharacter.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonCharacter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speedWalk = 3.0f;
+    public float speedJump = 6.0f;
+    Rigidbody2D dragonChar;
+
+    public bool betterJump = false;
+    public float fallMultiplier = 0.5f;
+
+    public float lowJumpMultiplier = 1f;
+
+    public SpriteRenderer spriteRen;
+    public Animator animator;
+
+    void Start()
+    {
+        dragonChar = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("d") || Input.GetKey("right")) {
+            dragonChar.velocity = new Vector2(speedWalk, dragonChar.velocity.y);
+            spriteRen.flipX = false;
+            animator.SetBool("Run", true);
+            animator.SetBool("Flykick", false);
+        }
+        else
+        {
+            if (Input.GetKey("a") || Input.GetKey("left"))
+            {
+                dragonChar.velocity = new Vector2(-speedWalk, dragonChar.velocity.y);
+                spriteRen.flipX = true;
+                animator.SetBool("Run", true);
+                animator.SetBool("Flykick", false);
+                if (Input.GetKey("space") && Input.GetKey("a"))
+                {
+                    animator.SetBool("Run", false);
+                    animator.SetBool("Flykick", true);
+                    dragonChar.velocity = new Vector2(-speedWalk-2.0f, dragonChar.velocity.y);
+                }
+            }
+            else
+            {
+                dragonChar.velocity = new Vector2(0, dragonChar.velocity.y);
+                animator.SetBool("Run", false);
+            }
+        }
+        if (Input.GetKey("space") && Input.GetKey("d"))
+        {
+            animator.SetBool("Run", false);
+            animator.SetBool("Flykick", true);
+            dragonChar.velocity = new Vector2(speedWalk + 2.0f, dragonChar.velocity.y);
+        }
+
+        /*if (Input.GetKey("w")) JumStrike  Flykick
+        {
+            dragonChar.velocity = new Vector2(0, speedJump);
+        }*/
+        if (Input.GetKey("w") && bock.isGround)
+        {
+            dragonChar.velocity = new Vector2(dragonChar.velocity.x, speedJump);
+
+        }
+        if(bock.isGround == false)
+        {
+            animator.SetBool("JumStrike", true);
+            animator.SetBool("Run", false);
+        }
+        if (bock.isGround == true)
+        {
+            animator.SetBool("JumStrike", false);
+        }
+
+        if (betterJump)
+        {
+            if (dragonChar.velocity.y < 0)
+            {
+                dragonChar.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier)*Time.deltaTime;
+            }
+            if (dragonChar.velocity.y > 0 && !Input.GetKey("w"))
+            {
+                dragonChar.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier) * Time.deltaTime;
+
+            }
+        }
+    }
+}
---
+++ My project JuegosInteractivos/Assets/Scripts/bock.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bock : MonoBehaviour
+{
+    public static bool isGround;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGround = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        isGround = false;
+    }
+
+}
